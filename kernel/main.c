322bee22741a710dcac7eb8a83f046282bd73822
HAPPY <pcy190@126.com>
2019-03-23 00:07:20 -0700

    kernel with put_char

---
+++ kernel/main.c
@@ -0,0 +1,10 @@
+#include "print.h"
+int main(){
+    put_char('k');
+    put_char('e');
+    put_char('r');
+    put_char('n');
+    put_char('e');
+    put_char('l');
+    put_char('\n');
+}
---
+++ lib/kernel/print.h
@@ -0,0 +1,8 @@
+#ifndef __LIB_KERNEL_PRINT_H
+#define __LIB_KERNEL_PRINT_H
+#include "stdint.h"
+void put_char(uint8_t char_asci);
+//void putstr(char* message);
+//void putint(uint32_t num);	 // print with HEX format
+#endif
+
